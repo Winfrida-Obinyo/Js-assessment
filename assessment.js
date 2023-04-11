44cbe535faca1fc33c6a402978211f83ee109f28
Winfida-Obinyo <obinyowinfridaa@gmail.com>
2023-04-11 09:03:00 +0300

    committed assessment

---
+++ assessment.js
@@ -0,0 +1,53 @@
+// // Write a function that takes in a string and returns it when reversed
+//  let food = “eating”
+ let food =["eating"]
+ if (food.reverse) {
+    console.log(food)
+ }   
+
+//  Write a function that takes in the following array and consoles the target if it is found else
+// null
+let num = [2,8,0,23,5,45,76]
+Target = 23
+function(arr,target){
+let leftIndex =0
+let rightIndex = arr.length
+
+
+}
+// Given years between 2000 and 2023, console all the leap years in the following
+// sentence, i.e “2020 is a leap year” if not console log i.e “2001 is not a leap year”
+let year = [2000,2023]
+if (year in 2000>2023) {
+    console.log(leap years)
+    
+}
+else{
+    console.log(2001 is not a leap year)
+}
+
+// Given a range of numbers from 0 to 100, console”Fizz” if the numbers are divisible by 3,
+// // “Buzz” if the numbers are divisible by 5, and “FizzBuzz” if divisible by both 3 and 5.
+let numbers = [0 to 100]
+let w = 0
+if (2%==0) {
+    console.log(fizz);
+    
+}
+
+// Write a function that takes in an array of numbers and returns an array that has all
+// elements multiplied by 4.
+
+let numArray = [12,87,45,75,23,64,73]
+let z = 0
+for (let i = 0; i < numArray.length; i*4) {
+    z*= numArray[i];
+    
+}
+// Write a function that takes in an array of strings and returns an array with every element
+// turned into a number
+let nums = [“10”,”24”,”45”,”56”,”67”]
+let y = 0  
+nums.forEach(element =>0 {
+    return element
+});
